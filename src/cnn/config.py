622d1eaae6d300fb4c5042576f9940c654260788
richardl2003 <richardliy03@gmail.com>
2024-12-02 22:01:09 -0500

    fix: config paths

--- src/cnn/config.py
+++ src/cnn/config.py
@@ -2,7 +2,7 @@ LEARNING_RATE = 0.001
 NUM_EPOCHS = 20
 CIFAR_10_CLASSES = ('airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 CIFAR_10_NUM_CLASSES = len(CIFAR_10_CLASSES)
-MODEL_PATH = lambda x: f"../../models/{x}.pt"
+MODEL_PATH = lambda x: f"../../models/{x}"
 BATCH_SIZE = 64
-CONFUSION_MATRIX_PATH = lambda x: f"../../results/{x}.png"
+CONFUSION_MATRIX_PATH = lambda x: f"../../results/{x}"
 
